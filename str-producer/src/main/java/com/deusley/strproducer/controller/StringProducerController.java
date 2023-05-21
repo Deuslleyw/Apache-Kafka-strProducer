0ab48f06aea0d2ec07ec056533e53eef0d3b57ef
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2023-05-21 02:00:36 -0300

    testeRequest=ok

--- str-producer/src/main/java/com/deusley/strproducer/controller/StringProducerController.java
+++ str-producer/src/main/java/com/deusley/strproducer/controller/StringProducerController.java
@@ -4,6 +4,7 @@ import com.deusley.strproducer.services.StringProducerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class StringProducerController {
 
     private final StringProducerService prodService;
 
+    @PostMapping
     public ResponseEntity <?> sendMessage(@RequestBody String message){
         prodService.sendMessage(message);
 
